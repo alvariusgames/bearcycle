970db95002dd851e06769c43c37dcf52c67bfae0
David Joseph Vitale <davidjosephvitale@gmail.com>
2017-07-25 20:32:42 -0500

    Got input working with finite state

--- main.gd
+++ main.gd
@@ -1,7 +1,13 @@
 extends Node
 
 func _ready():
-	self.set_process(true)
+	set_process_input(true)
 
-func _process(delta):
-	get_node("player").set_state(get_node("/root/STATE").PLAYER.MOVE_RIGHT)
+func _input(event):
+	if(event.type == InputEvent.KEY):
+		if(event.scancode == KEY_RIGHT):
+			get_node("player").set_state(\
+				get_node("/root/STATE").PLAYER.MOVE_RIGHT)
+		elif(event.scancode == KEY_LEFT):
+			get_node("player").set_state(\
+				get_node("/root/STATE").PLAYER.MOVE_LEFT)
