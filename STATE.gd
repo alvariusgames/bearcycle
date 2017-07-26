e094328cf7cf4adfac5cfbf1f09bd1a003177a02
David Joseph Vitale <davidjosephvitale@gmail.com>
2017-07-25 23:56:51 -0500

    4 states to the machine, smooth accelerating/idling/braking

--- STATE.gd
+++ STATE.gd
@@ -1,7 +1,9 @@
 extends Node
 
 const PLAYER =  { MOVE_RIGHT = 0,
-				  MOVE_LEFT = 1 }
+				  MOVE_LEFT = 1 ,
+				  IDLE = 2,
+				  BRAKE = 3 }
 
 func _ready():
 	pass
--- src/player.gd
+++ src/player.gd
@@ -1,21 +1,35 @@
 extends Node
 
 var _state
-var _pos
+var _pos = Vector2(50,50)
+var _speed = 0
+var _accel = 0.25
+var _max_speed = 10
+var _idle_effect = 0.95
+var _brake_effect = 0.85
 
 func _ready():
-	_pos = Vector2(0,0)
 	self.set_process(true)
 
 func set_state(state):
 	_state = state
 
 func react_to_state():
-	if _state == get_node("/root/STATE").PLAYER.MOVE_RIGHT:
-		_pos.x += 1
-	elif _state == get_node("/root/STATE").PLAYER.MOVE_LEFT:
-		_pos.x -= 1
+	if(_state == get_node("/root/STATE").PLAYER.MOVE_RIGHT):
+		if(_speed < _max_speed):
+			_speed += _accel
+	elif(_state == get_node("/root/STATE").PLAYER.MOVE_LEFT):
+		if(_speed > -_max_speed):
+			_speed -= _accel
+	elif(_state == get_node("/root/STATE").PLAYER.IDLE):
+			_speed = _speed * _idle_effect
+	elif(_state == get_node("/root/STATE").PLAYER.BRAKE):
+			_speed = _speed * _brake_effect
+
+func react_stateless():
+	_pos.x += _speed
+	self.get_node("player_sprite").set_pos(_pos)
 
 func _process(delta):
 	self.react_to_state()
-	self.get_node("player_sprite").set_pos(_pos)
+	self.react_stateless()
