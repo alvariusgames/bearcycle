8f1a486fe685fbbe003d0d64b4516d3ffb6d9b96
David Joseph Vitale <DavidJVitale@users.noreply.github.com>
2018-05-06 23:13:08 -0500

    Almost done, just need to tidy up..

--- src/Player.cs
+++ src/Player.cs
@@ -1,58 +1,77 @@
 using Godot;
 using System;
 
-public class Player : KinematicBody2D
-{
+public class Player : KinematicBody2D{
+    public Vector2 velocity = new Vector2(0,0);
+    public Sprite sprite;
+    private float forwardAccell = 0f;
+    //The below 2 vars auto update, & can be used to calculate all info about "forward"
+    // See `this.calculateForwardAngle()` for more information
+    private Vector2 currentTravel = new Vector2(0,0);
+    private Vector2 currentNormal = new Vector2(0,-1);
     private const float GRAVITY  = 600.0f;
     private const float MAX_GRAVITY_SPEED = 300f;
     private const float MAX_FORWARD_ACCEL = 40f;
     private const float MAX_BACKWARD_ACCEL = - MAX_FORWARD_ACCEL;
-    private Vector2 velocity = new Vector2(0,0);
     private const float MAX_SPEED = 300f;
-    private float forwardAccell = 0f;
     private const float FORWARD_ACCEL_UNIT = 1.5f;
     private const float DECELL_EFFECT = 0.9f;
-    private const float FRICTION_EFFECT = 0.90f;
+    private const float FRICTION_EFFECT = 0.9f;
 
-    public override void _PhysicsProcess(float delta)
-    {
+    public override void _Ready(){
+        for(int i = 0; i<this.GetChildCount(); i++){
+            var child = this.GetChild(i);
+            if(child is Sprite){
+                this.sprite = (Sprite)child;
+                return;
+            }
+        }
+        throw new Exception("No Sprite found as Child of Player");
+    }
+
+    public override void _PhysicsProcess(float delta){
         this.reactToInput(delta);
         this.processPhysics(delta);
         this.applyPhysics(delta);
+        this.updateSprite(delta);
     }
 
     private void reactToInput(float delta){
         if (Input.IsActionPressed("ui_left") &&
-           (this.forwardAccell >= MAX_BACKWARD_ACCEL))
-        {
+        (this.forwardAccell >= MAX_BACKWARD_ACCEL)){
             this.forwardAccell -= FORWARD_ACCEL_UNIT;
         }
         else if (Input.IsActionPressed("ui_right") &&
-                (this.forwardAccell <= MAX_FORWARD_ACCEL)) 
-        {       
+        (this.forwardAccell <= MAX_FORWARD_ACCEL)) {       
             this.forwardAccell += FORWARD_ACCEL_UNIT;
         }
-        else
-        {
+        else{
             this.forwardAccell *= DECELL_EFFECT;
         }
     }
 
     private void processPhysics(float delta){
-        //GD.Print(this.velocity);
-        //Apply the gravity effect
+        //Process Gravity
         if(this.velocity.y <= MAX_GRAVITY_SPEED){        
             this.velocity.y += delta * GRAVITY;
         }
 
+        //Process Collision with platforms
+        var numCollisions = this.GetSlideCount();
         for(int i = 0; i < this.GetSlideCount(); i++){
-            //Apply forward speed ONLY during collisions
-           var forwardAngle = this.calculateForwardAngle(GetSlideCollision(i));
+           var collision = this.GetSlideCollision(i);
+           //Save relevant collision info to this
+           this.currentTravel = collision.Travel;
+           this.currentNormal = collision.Normal;
+           //Calculate the Forward movement
+           var forwardAngle = this.calculateForwardAngle();
            if (Math.Abs(this.velocity.Length()) <= MAX_SPEED){
                 this.velocity.x += forwardAngle.x*forwardAccell;
                 this.velocity.y += forwardAngle.y*forwardAccell;
             }
+            //Apply the friction effect
             this.velocity *= FRICTION_EFFECT;
+            break; //For now, only process 1 collision (buggy with multiple atm)
         }
     }
 
@@ -62,36 +81,27 @@ public class Player : KinematicBody2D
     ///     - We must calculate this
     /// Purely mathematically, Forward = Normal + (PI / 2 radians)
     /// In this Godot project, this works for Convex curves, but fails for Concave curves
-    ///     - The Player will follow the curve when it should fly away from it
-    /// The solution to this is to make the "Forward" angle a bit "above" forward.
-    /// "above" is defined as the direction towards "Normal" (away from the platform)
-    /// "above" is dependant upon what direction the player is currently moving, AKA the angle of velocity
-    private Vector2 calculateForwardAngle(KinematicCollision2D collision){
-        //Calculate the percent offset angle above
+    ///     - The Player will follow the curve when it should "fly away" from it
+    /// The solution to this is to make the angle a bit "above" the traditional "forward".
+    ///     - "above" is defined as the direction towards "Normal" (away from the platform)
+    ///     - "above" is dependant the direction player is moving (AKA the angle of velocity)
+    private Vector2 calculateForwardAngle(){
+        //Calculate the angle above foreward
         const float angleAbovePercent = 0.20f;
-        var normalAngle = collision.Normal.Normalized();
-        var currentVelocityAngle = collision.Travel.Normalized();
+        var normalAngle = this.currentNormal.Normalized();
+        var currentVelocityAngle = this.currentTravel.Normalized();
         var angleAbove = angleAbovePercent * currentVelocityAngle.AngleTo(normalAngle);
-        //Apply that angle to the forward angle 
+        //Apply the previously calculate "angleAbove" to the forward angle 
         var unadjustedForwardAngle = normalAngle.Rotated((float)Math.PI / 2f).Normalized();
         var adjustedForwardAngle = unadjustedForwardAngle.Rotated(angleAbove);
-        GD.Print(angleAbove);
         return adjustedForwardAngle;
-   }
+    }
     private void applyPhysics(float delta){
         MoveAndSlide(linearVelocity: this.velocity);
     }
-            
-            //col.Colli((CollisionPolygon2D)col.ColliderShape).Polygon[col.ColliderShapeIndex];
-        /*
-        var remainingVelocity = MoveAndSlide(new Vector2(0, 10 * delta * gravity), new Vector2(0,-1), 19, 19, 2f * (float)Math.PI);
-        var floorAngle = (remainingVelocity - velocity).Normalized();
-        var forwardAngle = floorAngle.Angle() + ((float)Math.PI / 2f);
-        var lostVelocity = new Vector2((float)Math.Cos(forwardAngle)*forwardSpeed,
-                                       (float) Math.Sin(forwardAngle)*forwardSpeed);
-        var leftover = MoveAndSlide(lostVelocity, floorAngle,19, 19, 2f*(float)Math.PI);
-        GD.Print(floorAngle);
-        */
+
+    private void updateSprite(float delta){
+        const float arbitraryConstant = 400;
+        this.sprite.Rotate(this.forwardAccell / arbitraryConstant);
+    }
 }
-
-
