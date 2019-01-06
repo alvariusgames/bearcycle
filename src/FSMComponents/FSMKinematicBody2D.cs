ba8b2697ee9ba5c21cba0993df2d00028b624c88
David Joseph Vitale <DavidJVitale@users.noreply.github.com>
2019-01-05 18:27:09 -0600

    Special graphic for front wheel, adds a timer functionality to base FSM class

--- src/FSMComponents/FSMKinematicBody2D.cs
+++ src/FSMComponents/FSMKinematicBody2D.cs
@@ -6,28 +6,46 @@ using System.Linq;
 public abstract class FSMKinematicBody2D<StateEnum> : KinematicBody2D, IFSMObject<StateEnum> {
     public StateEnum ActiveState {get; private set;}
     public int ActiveStatePriority {get; private set;}
-
-    private SortedList<int, StateEnum> prevActiveStates = new SortedList<int, StateEnum>();
-
     public void SetActiveState(StateEnum ActiveState, int priority){
-        if(priority >= this.ActiveStatePriority){
-            this.prevActiveStates[this.ActiveStatePriority] = this.ActiveState;
+        if(priority >= ActiveStatePriority){
             this.ActiveState = ActiveState;
-            this.ActiveStatePriority = priority;
-        }
-    }
+            this.ActiveStatePriority = priority;}}
 
     public void ResetActiveState(StateEnum ActiveState){
         this.ActiveStatePriority = 0;
-        this.ActiveState = ActiveState;
-    }
-   public abstract void UpdateState(float delta);
+        this.ActiveState = ActiveState;}
+
+    private struct TimerAttrs{
+        public float secondsCount;
+        public float numSecondsToTriggerStateChange;
+        public StateEnum stateToSetTo;
+        public int priorityToSetTo;
+        public TimerAttrs(float numSecondsToTriggerStateChange,
+                          StateEnum stateToSetTo,
+                          int priorityToSetTo){
+            this.secondsCount = 0f;
+            this.numSecondsToTriggerStateChange = numSecondsToTriggerStateChange;
+            this.stateToSetTo = stateToSetTo;
+            this.priorityToSetTo = priorityToSetTo;}}
+    private List<TimerAttrs> timersSet = new List<TimerAttrs>();
+
+    public void SetActiveStateAfter(StateEnum ActiveState, int priority, float numSeconds){
+        this.timersSet.Add(new TimerAttrs(numSeconds, ActiveState, priority));}
+
+    private void handleTimers(float delta){
+        for(var i=0; i<this.timersSet.Count(); i++){
+            var timerAttr = this.timersSet[i];
+            timerAttr.secondsCount += delta;
+            if(timerAttr.secondsCount >= timerAttr.numSecondsToTriggerStateChange){
+                this.SetActiveState(timerAttr.stateToSetTo, timerAttr.priorityToSetTo);
+                this.timersSet.Remove(timerAttr);}}}
+
+    public abstract void UpdateState(float delta);
     public abstract void ReactStateless(float delta);
     public abstract void ReactToState(float delta);
 
-    public override void _PhysicsProcess(float delta) {
+    public override void _Process(float delta) {
         this.UpdateState(delta);
-        this.ReactToState(delta);
         this.ReactStateless(delta);
-    }
-}
+        this.handleTimers(delta);
+        this.ReactToState(delta);}}
--- src/FSMComponents/FSMNode2D.cs
+++ src/FSMComponents/FSMNode2D.cs
@@ -6,28 +6,46 @@ using System.Linq;
 public abstract class FSMNode2D<StateEnum> : Node2D, IFSMObject<StateEnum> {
     public StateEnum ActiveState {get; private set;}
     public int ActiveStatePriority {get; private set;}
-    private Dictionary<int, StateEnum> prevActiveStates = new Dictionary<int, StateEnum>();
-
     public void SetActiveState(StateEnum ActiveState, int priority){
         if(priority >= ActiveStatePriority){
-            this.prevActiveStates[this.ActiveStatePriority] = this.ActiveState;
             this.ActiveState = ActiveState;
-            this.ActiveStatePriority = priority;
-        }
-    }
+            this.ActiveStatePriority = priority;}}
 
     public void ResetActiveState(StateEnum ActiveState){
         this.ActiveStatePriority = 0;
-        this.ActiveState = ActiveState;
-    }
+        this.ActiveState = ActiveState;}
 
-   public abstract void UpdateState(float delta);
+    private struct TimerAttrs{
+        public float secondsCount;
+        public float numSecondsToTriggerStateChange;
+        public StateEnum stateToSetTo;
+        public int priorityToSetTo;
+        public TimerAttrs(float numSecondsToTriggerStateChange,
+                          StateEnum stateToSetTo,
+                          int priorityToSetTo){
+            this.secondsCount = 0f;
+            this.numSecondsToTriggerStateChange = numSecondsToTriggerStateChange;
+            this.stateToSetTo = stateToSetTo;
+            this.priorityToSetTo = priorityToSetTo;}}
+    private List<TimerAttrs> timersSet = new List<TimerAttrs>();
+
+    public void SetActiveStateAfter(StateEnum ActiveState, int priority, float numSeconds){
+        this.timersSet.Add(new TimerAttrs(numSeconds, ActiveState, priority));}
+
+    private void handleTimers(float delta){
+        for(var i=0; i<this.timersSet.Count(); i++){
+            var timerAttr = this.timersSet[i];
+            timerAttr.secondsCount += delta;
+            if(timerAttr.secondsCount >= timerAttr.numSecondsToTriggerStateChange){
+                this.SetActiveState(timerAttr.stateToSetTo, timerAttr.priorityToSetTo);
+                this.timersSet.Remove(timerAttr);}}}
+
+    public abstract void UpdateState(float delta);
     public abstract void ReactStateless(float delta);
     public abstract void ReactToState(float delta);
 
     public override void _Process(float delta) {
         this.UpdateState(delta);
         this.ReactStateless(delta);
-        this.ReactToState(delta);
-    }
-}
+        this.handleTimers(delta);
+        this.ReactToState(delta);}}
